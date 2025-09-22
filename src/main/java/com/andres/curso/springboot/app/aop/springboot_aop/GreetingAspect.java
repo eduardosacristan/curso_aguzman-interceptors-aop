1a07ee3bc22e5745ad9771480f7a8172f27a49b0
eduardosacristan <eduardosacristan@outlook.es>
2025-09-22 09:47:43 +0100

    Implementato el around

--- src/main/java/com/andres/curso/springboot/app/aop/springboot_aop/GreetingAspect.java
+++ src/main/java/com/andres/curso/springboot/app/aop/springboot_aop/GreetingAspect.java
@@ -3,6 +3,8 @@ package com.andres.curso.springboot.app.aop.springboot_aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,12 +17,32 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(String com.andres.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    @Before("execution(* com.andres.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") //Podemos usar el * como comodín
     public void loggerBefore(JoinPoint joinPoint) {
         String metodo = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
 
         logger.info("Antes ".concat(metodo).concat(" con los argumentos ".concat(args)));
+    }
 
+    @After("execution(* com.andres.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") //Podemos usar el * como comodín
+    public void loggerAfter(JoinPoint joinPoint) {
+        String metodo = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Después ".concat(metodo).concat(" con los argumentos ".concat(args)));
+    }
+
+    /*
+     * Estos dos aspects envuelven al método sayHello del controlador.
+     * También podemos envolverlo con Around
+     */
+
+    @Around("execution(* com.andres.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") //Podemos usar el * como comodín
+    public void loggerAround(JoinPoint joinPoint) {
+        String metodo = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Antes y después ".concat(metodo).concat(" con los argumentos ".concat(args)));
     }
 }
